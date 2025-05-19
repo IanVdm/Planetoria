a54ccff28186df44ba3124757afcad7fba4d60b9
Ian van der Merwe <ianvdm2001@gmail.com>
2025-05-19 09:00:07 +0200

    Initial Commit

---
+++ Planetoria/PlanetoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetoriaApp.swift
+//  Planetoria
+//
+//  Created by Ian Van der Merwe on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanetoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
